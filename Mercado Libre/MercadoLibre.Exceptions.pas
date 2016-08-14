43e1a826830cbdcd0c934f690fa13bb10cd18cc0
Agustin Ortu <ortu.agustin@gmail.com>
2016-08-14 02:54:42 -0300

    Mercado Libre API: EInvalidToken exception

--- Mercado Libre/MercadoLibre.Exceptions.pas
+++ Mercado Libre/MercadoLibre.Exceptions.pas
@@ -12,6 +12,26 @@ type
   /// <summary> Raised when an Authorization fails </summary>
   EAuthorizationFailed = class(EMercadoLibreException);
 
+  /// <summary> Raised when failed to get an Access Token </summary>
+  EInvalidToken = class(EMercadoLibreException)
+  private
+    FJSON: string;
+//    function GetErrorMessage: string;
+  public
+    constructor Create(const RawJSON: string);
+//    property ErrorMessage: string read GetErrorMessage;
+  end;
+
 implementation
+
+{$REGION 'EInvalidToken'}
+
+constructor EInvalidToken.Create(const RawJSON: string);
+begin
+  inherited Create(RawJSON);
+  FJSON := RawJSON;
+end;
+
+{$ENDREGION}
 
 end.
