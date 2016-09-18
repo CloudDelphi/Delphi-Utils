52c6dee066404d17a58a158b7644e3bc05b74192
Agustin Ortu <ortu.agustin@gmail.com>
2016-09-18 00:51:48 -0300

    Vcl.CustomDialogs: Cleanup

--- Vcl/Vcl.CustomDialogs.pas
+++ Vcl/Vcl.CustomDialogs.pas
@@ -8,46 +8,80 @@ unit Vcl.CustomDialogs;
 
 interface
 
+{$REGION 'Information Messages'}
 /// <summary> Shows an Information Message </summary>
-procedure InfoMsg(const Msg: string);
-/// <summary> Shows an Information Message with custom formatting </summary>
-procedure InfoMsgFmt(const Msg: string; const Args: array of const);
-/// <summary> Displays a Confirmation Message with a custom formatted prompt (OK-Cancel). Returns True if click on OK. False otherwise </summary>
-function PromptMsg(const Prompt: string): Boolean;
-/// <summary> Displays a Confirmation Message with a prompt (OK-Cancel). Returns True if click on OK. False otherwise </summary>
-function PromptMsgFmt(const Prompt: string; const AParams: array of const): Boolean;
-/// <summary> Displays a Confirmation Message prompting to discard changes (OK-Cancel). Returns True if click on OK. False otherwise </summary>
-function PromptDiscardAndExit: Boolean;
-/// <summary> Displays a Error Message with a custom formatted prompt (OK-Cancel). Returns True if click on OK. False otherwise</summary>
-function ErrorAndPromptMsg(const Prompt: string): Boolean;
+procedure InfoMsg(const Msg: string); overload;
+/// <summary> Shows an Information Message </summary>
+procedure InfoMsg(const Msg: string; const FormatArgs: array of const); overload;
+{$ENDREGION}
+
+{$REGION 'Confirmation Messages'}
+/// <summary> Displays a Confirmation Message with a prompt. Returns True if click on OK. False otherwise </summary>
+function PromptMsg(const Prompt: string): Boolean; overload;
+/// <summary> Displays a Confirmation Message with a prompt. Returns True if click on OK. False otherwise </summary>
+function PromptMsg(const Prompt: string; const FormatArgs: array of const): Boolean; overload;
+/// <summary> Displays a PromptMsg with a Checkbox </summary>
+function CheckPromptMsg(const Prompt, CheckPrompt: string; var Checked: Boolean): Boolean; overload;
+/// <summary> Displays a PromptMsg with a Checkbox </summary>
+function CheckPromptMsg(const Prompt, CheckPrompt: string; const FormatArgs: array of const;
+  var Checked: Boolean): Boolean; overload;
+/// <summary> PromptMsg asking to discard changes. Returns True if click on OK. False otherwise </summary>
+function PromptDiscardAndExit: Boolean; overload;
+{$ENDREGION}
+
+{$REGION 'Error Messages'}
+/// <summary> Displays a Error Message. Returns True if click on OK. False otherwise </summary>
+function ErrorPromptMsg(const Prompt: string): Boolean; overload;
+/// <summary> Displays a Error Message. Returns True if click on OK. False otherwise </summary>
+function ErrorPromptMsg(const Prompt: string; const FormatArgs: array of const): Boolean; overload;
 /// <summary> Shows an Error Message </summary>
-procedure ErrorMsg(const Msg: string);
-/// <summary> Shows an Error Message with custom formatting </summary>
-procedure ErrorMsgFmt(const Msg: string; const Args: array of const);
+procedure ErrorMsg(const Msg: string); overload;
+/// <summary> Shows an Error Message </summary>
+procedure ErrorMsg(const Msg: string; const FormatArgs: array of const); overload;
 /// <summary> Shows an Error Message and then calls System.Abort </summary>
-procedure ErrorAbortMsg(const Msg: string);
-/// <summary> Shows an Error Message with custom formatting and then calls System.Abort </summary>
-procedure ErrorAbortMsgFmt(const Msg: string; const Args: array of const);
-/// <summary> Displays a Warning Message </summary>
-function WarningMsg(const Msg: string; const ShowCancelButton: Boolean = False): Integer;
-/// <summary> Displays a Warning Message with custom formatting </summary>
-function WarningMsgFmt(const Msg: string; const Args: array of const; const ShowCancelButton: Boolean = False): Integer;
-/// <summary> Displays a Warning Message with a prompt (OK-Cancel). Returns True if click on OK. False otherwise </summary>
-function WarningPrompt(const Prompt: string): Boolean;
-/// <summary> Displays a Warning Message with a custom formatted prompt (OK-Cancel). Returns True if click on OK. False otherwise </summary>
-function WarningPromptFmt(const Prompt: string; const Args: array of const): Boolean;
+procedure ErrorAbortMsg(const Msg: string); overload;
+/// <summary> Shows an Error Message and then calls System.Abort </summary>
+procedure ErrorAbortMsg(const Msg: string; const FormatArgs: array of const); overload;
+{$ENDREGION}
+
+{$REGION 'Warning Messages'}
+/// <summary> Shows an Warning Message </summary>
+procedure WarningMsg(const Msg: string); overload;
+/// <summary> Shows an Warning Message </summary>
+procedure WarningMsg(const Msg: string; const FormatArgs: array of const); overload;
+/// <summary> Displays a Warning Message with a prompt. Returns True if click on OK. False otherwise </summary>
+function WarningPrompt(const Prompt: string): Boolean; overload;
+/// <summary> Displays a Warning Message with a prompt. Returns True if click on OK. False otherwise </summary>
+function WarningPrompt(const Prompt: string; const FormatArgs: array of const): Boolean; overload;
+{$ENDREGION}
+
+procedure PromptOpenFile(const Prompt, Filename: string);
 
 implementation
 
 uses
-  {$IF CompilerVersion > 21}
-  System.SysUtils,
-  Vcl.Dialogs,
-  {$ELSE}
+  Vcl.CustomMsgBox,
   SysUtils,
   Dialogs,
-  {$IFEND}
-  Vcl.CustomMsgBox;
+  Windows,
+  ShellAPI;
+
+{$REGION 'Internal use'}
+
+function DoWarningMsg(const Msg: string; const ShowCancelButton: Boolean): Boolean; overload;
+begin
+  if ShowCancelButton then
+    Result := MsgBox('Advertencia', Msg, mtWarning, ['Aceptar', 'Cancelar']) = 0
+  else
+    Result := MsgBox('Advertencia', Msg, mtWarning, ['Aceptar']) = 0
+end;
+
+function DoWarningMsg(const Msg: string; const FormatArgs: array of const; const ShowCancelButton: Boolean): Boolean; overload;
+begin
+  Result := DoWarningMsg(Format(Msg, FormatArgs), ShowCancelButton);
+end;
+
+{$ENDREGION}
 
 procedure ErrorAbortMsg(const Msg: string);
 begin
@@ -55,14 +89,14 @@ begin
   Abort;
 end;
 
-procedure ErrorAbortMsgFmt(const Msg: string; const Args: array of const);
+procedure ErrorAbortMsg(const Msg: string; const FormatArgs: array of const);
 begin
-  ErrorAbortMsg(Format(Msg, Args));
+  ErrorAbortMsg(Format(Msg, FormatArgs));
 end;
 
-procedure ErrorMsgFmt(const Msg: string; const Args: array of const);
+procedure ErrorMsg(const Msg: string; const FormatArgs: array of const);
 begin
-  ErrorMsg(Format(Msg, Args));
+  ErrorMsg(Format(Msg, FormatArgs));
 end;
 
 procedure InfoMsg(const Msg: string);
@@ -70,9 +104,9 @@ begin
   MsgBox('Información', Msg, mtInformation, ['Aceptar']);
 end;
 
-procedure InfoMsgFmt(const Msg: string; const Args: array of const);
+procedure InfoMsg(const Msg: string; const FormatArgs: array of const);
 begin
-  InfoMsg(Format(Msg, Args));
+  InfoMsg(Format(Msg, FormatArgs));
 end;
 
 function PromptMsg(const Prompt: string): Boolean;
@@ -80,9 +114,20 @@ begin
   Result := MsgBox('Confirmar', Prompt, mtConfirmation, ['Si', 'No']) = 0;
 end;
 
-function PromptMsgFmt(const Prompt: string; const AParams: array of const): Boolean;
+function PromptMsg(const Prompt: string; const FormatArgs: array of const): Boolean;
 begin
-  Result := PromptMsg(Format(Prompt, AParams));
+  Result := PromptMsg(Format(Prompt, FormatArgs));
+end;
+
+function CheckPromptMsg(const Prompt, CheckPrompt: string; var Checked: Boolean): Boolean;
+begin
+  Result := MsgBox('Confirmar', Prompt, mtConfirmation, ['Si', 'No'], CheckPrompt, Checked) = 0;
+end;
+
+function CheckPromptMsg(const Prompt, CheckPrompt: string; const FormatArgs: array of const;
+  var Checked: Boolean): Boolean;
+begin
+  Result := CheckPromptMsg(Format(Prompt, FormatArgs), CheckPrompt, Checked);
 end;
 
 function PromptDiscardAndExit: Boolean;
@@ -90,9 +135,14 @@ begin
   Result := PromptMsg('Descartar cambios y salir?');
 end;
 
-function ErrorAndPromptMsg(const Prompt: string): Boolean;
+function ErrorPromptMsg(const Prompt: string): Boolean;
 begin
   Result := MsgBox('Error', Prompt, mtError, ['Aceptar', 'Cancelar']) = 0;
+end;
+
+function ErrorPromptMsg(const Prompt: string; const FormatArgs: array of const): Boolean;
+begin
+  Result := ErrorPromptMsg(Format(Prompt, FormatArgs));
 end;
 
 procedure ErrorMsg(const Msg: string);
@@ -100,28 +150,30 @@ begin
   MsgBox('Error', Msg, mtError, ['Aceptar']);
 end;
 
-function WarningMsgFmt(const Msg: string; const Args: array of const;
-  const ShowCancelButton: Boolean): Integer;
+procedure WarningMsg(const Msg: string);
 begin
-  Result := WarningMsg(Format(Msg, Args), ShowCancelButton);
+  DoWarningMsg(Msg, False);
 end;
 
-function WarningMsg(const Msg: string; const ShowCancelButton: Boolean): Integer;
+procedure WarningMsg(const Msg: string; const FormatArgs: array of const);
 begin
-  if ShowCancelButton then
-    Result := MsgBox('Advertencia', Msg, mtWarning, ['Aceptar', 'Cancelar'])
-  else
-    Result := MsgBox('Advertencia', Msg, mtWarning, ['Aceptar'])
+  DoWarningMsg(Msg, FormatArgs, False);
 end;
 
 function WarningPrompt(const Prompt: string): Boolean;
 begin
-  Result := WarningMsg(Prompt, True) = 0;
+  Result := DoWarningMsg(Prompt, True);
 end;
 
-function WarningPromptFmt(const Prompt: string; const Args: array of const): Boolean;
+function WarningPrompt(const Prompt: string; const FormatArgs: array of const): Boolean;
 begin
-  Result := WarningPrompt(Format(Prompt, Args));
+  Result := WarningPrompt(Format(Prompt, FormatArgs));
+end;
+
+procedure PromptOpenFile(const Prompt, Filename: string);
+begin
+  if PromptMsg(Prompt + sLineBreak + sLineBreak + 'Abrir archivo?') then
+    ShellExecute(0, 'OPEN', PWideChar(Filename), nil, nil, SW_SHOWMAXIMIZED);
 end;
 
 end.
