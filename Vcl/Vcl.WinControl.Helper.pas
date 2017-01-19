ad100b2a63cc82100289ccf50a6ff81ec06f27f4
Agustin Ortu <ortu.agustin@gmail.com>
2017-01-18 22:56:28 -0300

    TWinControlHelper: AnyFocused static method

--- Vcl/Vcl.WinControl.Helper.pas
+++ Vcl/Vcl.WinControl.Helper.pas
@@ -12,6 +12,12 @@ type
     /// <summary> Intenta darle foco al control; devuelve True si tuvo exito, False en caso contrario </summary>
     /// <remarks> Este metodo captura y controla excepciones de tipo System.Classes.EInvalidOperation </remarks>
     function TrySetFocus: Boolean;
+    /// <summary> Devuelve True si alguno de los controles del arreglo tiene foco; False en caso contrario </summary>
+    class function AnyFocused(const Controls: TArray<TWinControl>): Boolean; overload;
+    /// <summary> Devuelve True si alguno de los controles del arreglo tiene foco; False en caso contrario
+    /// En caso de devolver True, el parametro de salida FocusedControl es asignado al primer control del arreglo
+    /// que tiene foco </summary>
+    class function AnyFocused(const Controls: TArray<TWinControl>; out FocusedControl: TWinControl): Boolean; overload;
   end;
 {$ENDREGION}
 
@@ -21,6 +27,31 @@ uses
   System.Classes;
 
 {$REGION 'TWinControlHelper'}
+
+class function TWinControlHelper.AnyFocused(const Controls: TArray<TWinControl>): Boolean;
+var
+  Dummy: TWinControl;
+begin
+  Result := TWinControl.AnyFocused(Controls, Dummy);
+end;
+
+class function TWinControlHelper.AnyFocused(const Controls: TArray<TWinControl>;
+  out FocusedControl: TWinControl): Boolean;
+var
+  Each: TWinControl;
+begin
+  for Each in Controls do
+  begin
+    if Each.Focused then
+    begin
+      FocusedControl := Each;
+      Exit(True);
+    end;
+  end;
+
+  FocusedControl := nil;
+  Result := False;
+end;
 
 function TWinControlHelper.TrySetFocus: Boolean;
 begin
